3247654e9e8f393c95f47afb9416187e8b289b76
Joao Oliveira <jbcoliveira1984@gmail.com>
2017-08-16 23:29:15 -0300

    Remove produto

--- produto-lista.php
+++ produto-lista.php
@@ -4,6 +4,12 @@ include("cabecalho.php");
 include("conecta.php");
 include("banco-produto.php");
 
+
+if(array_key_exists("removido", $_GET) && $_GET['removido']=='true') {
+?>
+    <p class="text-success">Produto <?=$id?> removido!</p>
+<?php
+}
 $produtos = listaProduto($conexao);
 ?>
 <table class="table table-striped table-bordered">
@@ -14,6 +20,7 @@ $produtos = listaProduto($conexao);
         <tr>
             <td><?= $produto['nome'] ?></td>
             <td><?= $produto['preco'] ?></td>
+            <td><a class="btn btn-danger" href="remove-produto.php?id=<?=$produto['id'] ?>">Remover</a></td>
         </tr>
 
     <?php
